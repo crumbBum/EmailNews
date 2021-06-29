b7eef05020059ec78e9f1c1339eafa8d6843fcd9
crumbBum <rjardel@atcoautomation.com>
2021-06-28 23:09:35 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import time
+import datetime
+import yagmail
+import pandas as pd
+from news import NewsFeed
+
+
+user = ''
+password = ''
+
+
+def send_email():
+    today = datetime.datetime.now().strftime('%Y-%m-%d')
+    yesterday = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+    news_feed = NewsFeed(interest=row['interest'],
+                         from_date=yesterday,
+                         to_date=today,
+                         language='en')
+    email = yagmail.SMTP(user=user, password=password)
+    email.send(to=row['email'],
+               subject=f"Check out what's happening in the news about {row['interest'].title()}.",
+               contents=f"Hey {row['name'].title()},\n See whats new in {row['interest']}. \n \n {news_feed.get()}")
+
+
+while True:
+    if datetime.datetime.now().hour == 24 and datetime.datetime.now().minute == 59:
+        df = pd.read_excel('people.xlsx')
+
+        for index, row in df.iterrows():
+            send_email()
+
+    time.sleep(60)
+
